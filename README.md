1b649873e9357a6e0bf7d52af2ada7c6091fd6bc
Olivia <olivia.phuonganh@gmail.com>
2021-09-26 14:33:11 +1000

    A simple CRUD mutation

--- README.md
+++ README.md
@@ -6,12 +6,11 @@ GraphQL is the rising star of backend technologies. It replaces REST as an API d
 https://www.howtographql.com/graphql-js/1-getting-started/
 The goal of this tutorial is to build an API for a Hacker News clone. 
 
-# Technologies:
+# Technologies
 
-Apollo Server 2.18: Fully-featured GraphQL Server with focus on easy setup, performance and great developer experience, graphql-js and more.
-Prisma: Replaces traditional ORMs. Use Prisma Client to access your database inside of GraphQL resolvers.
-GraphQL Playground: A “GraphQL IDE” that allows you to interactively explore the functionality of a GraphQL API by sending queries and mutations to it. It’s somewhat similar to Postman which offers comparable functionality for REST APIs. Among other things, GraphQL Playground:
-
-Auto-generates comprehensive documentation for all available API operations.
-Provides an editor where you can write queries, mutations & subscriptions, with auto-completion(!) and syntax highlighting.
-Lets you easily share your API operations.
+- Apollo Server 2.18: Fully-featured GraphQL Server with focus on easy setup, performance and great developer experience, graphql-js and more.
+- Prisma: Replaces traditional ORMs. Use Prisma Client to access your database inside of GraphQL resolvers.
+- GraphQL Playground: A “GraphQL IDE” that allows you to interactively explore the functionality of a GraphQL API by sending queries and mutations to it. It’s somewhat similar to Postman which offers comparable functionality for REST APIs. Among other things, GraphQL Playground:
+    - Auto-generates comprehensive documentation for all available API operations.
+    - Provides an editor where you can write queries, mutations & subscriptions, with auto-completion(!) and syntax highlighting.
+    - Lets you easily share your API operations.
--- src/index.js
+++ src/index.js
@@ -1,28 +1,80 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require("apollo-server");
 
-// 1
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
-// The exclamation mark in the type definition means that this field is required and can never be null.
-
-// 2
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+  {
+    id: "link-1",
+    url: "www.indexone.com",
+    description: "Item indexed one",
+  },
+  {
+    id: "link-2",
+    url: "www.indextwo.com",
+    description: "Item index 2",
+  },
+];
+/*
+You’re adding a new integer variable that simply 
+serves as a very rudimentary(thô sơ) way to generate unique IDs 
+for newly created Link elements.
+*/
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
-}
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    /*
+The implementation of the post resolver first creates a new link object, 
+then adds it to the existing links list and finally returns the new link.
+*/
+    post: (parent, args) => {
+      // args carries the arguments for the operation –
+      // in this case the url and description of the Link to be created.
+      let idCount = links.length;
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      let targetIndex = links.findIndex((x) => x.id == args.id);
+      if (targetIndex) {
+        if (args.url) {
+          links[targetIndex].url = args.url;
+        }
+        if (args.description) {
+          links[targetIndex].description = args.description;
+        }
+
+        return links[targetIndex];
+      }
+    },
+    deleteLink: (parent, args) => {
+      let targetIndex = links.findIndex((x) => x.id == args.id);
+      links[targetIndex].url = ""
+      links[targetIndex].description = ""
+      console.log(links[targetIndex])
+      return links[targetIndex]
+    }
+  },
+};
+
+const fs = require("fs");
+const { argsToArgsConfig } = require("graphql/type/definition");
+const path = require("path");
 
 // 3
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
-})
+});
 
-server
-  .listen()
-  .then(({ url }) =>
-    console.log(`Server is running on ${url}`)
-  );
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
